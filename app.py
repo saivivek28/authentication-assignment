0827263f9d6b5509bf43c24014aa09eb98a92d70
vivek <saiviveknakirikanti@gmail.com>
2025-08-21 14:54:04 +0530

    authoriazation updated with custom decorater

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, request, jsonify
 from flask_migrate import Migrate
 from models import db, User
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import JWTManager, create_access_token, decode_token, get_jwt_identity, jwt_required
+from functools import wraps
 
 app = Flask(__name__)
 migrate = Migrate(app, db)
@@ -22,6 +23,24 @@ class Config():
 app.config.from_object(Config)
 db.init_app(app)
 
+def auth_required(f):
+    @wraps(f)
+    def user_auth(*args, **kwargs):
+        authorization = request.headers.get("Authorization")
+        if authorization:
+            bearer, token = authorization.split()
+            decoded_token = decode_token(token)
+            user_email = decoded_token['sub']
+            user_details = User.query.filter_by(Email = user_email).first()
+            if not user_details:
+                return jsonify({"Err":"User not found"}), 401
+            
+            request.current_user = user_details
+            return f(*args, **kwargs)
+        else:
+            return jsonify({"message": "Please provide Authorization to done the action"}), 401
+    return user_auth
+            
 
 @app.route('/register', methods = ["POST"])
 def register():
@@ -63,51 +82,63 @@ def login():
     
     
 @app.route('/profile')
-@jwt_required()
+@auth_required
 def profile():
-    user_email = get_jwt_identity()
-    print(user_email)
-    user_details = User.query.filter_by(Email=user_email).first()
-    if not user_details:
-        return "user not found"
-    print(user_details)
+    # user_email = get_jwt_identity()
+    # print(user_email)
+    # print(user_email)
+    # user_details = User.query.filter_by(Email=user_email).first()
+    # if not user_details:
+    #     return "user not found"
+    # print(user_details)
     
+    # return jsonify({
+    #     "Name":user_details.Name,
+    #     "Email":user_details.Email,
+    #     "Age":user_details.Age
+        
+    # }), 200
+    current_user = request.current_user
+    print(current_user)
     return jsonify({
-        "Name":user_details.Name,
-        "Email":user_details.Email,
-        "Age":user_details.Age
+        "Name":current_user.Name,
+        "Email":current_user.Email,
+        "Age":current_user.Age
         
     }), 200
     
+    
 @app.route('/edit',methods = ["PUT"])
-@jwt_required()
+@auth_required
 def edit():
     
-    user_email = get_jwt_identity()
-    user_details = User.query.filter_by(Email = user_email).first()
-    if not user_details:
-        return "User not found"
+    # user_email = get_jwt_identity()
+    # user_details = User.query.filter_by(Email = user_email).first()
+    # if not user_details:
+    #     return "User not found"
+    current_user = request.current_user
     updated_details = request.get_json()
     
-    user_details.Name = updated_details["Name"]
-    user_details.Age = updated_details["Age"]
+    current_user.Name = updated_details["Name"]
+    current_user.Age = updated_details["Age"]
     db.session.commit()
     return jsonify({
         "Message":"User details edited!",
-        "Name": user_details.Name,
-        "Email": user_email,
-        "Age":user_details.Age
+        "Name": current_user.Name,
+        "Email": current_user.Email,
+        "Age":current_user.Age
     })
     
     
 @app.route('/delete', methods = ["DELETE"])
-@jwt_required()
+@auth_required
 def delete():
-    user_email =get_jwt_identity()
-    user_details = User.query.filter_by(Email = user_email).first()
-    if not user_details:
-        return "User not found"
-    db.session.delete(user_details)
+    # user_email =get_jwt_identity()
+    # user_details = User.query.filter_by(Email = user_email).first()
+    # if not user_details:
+    #     return "User not found"
+    current_user = request.current_user
+    db.session.delete(current_user)
     db.session.commit()
     return "User Details Deleted"
     
